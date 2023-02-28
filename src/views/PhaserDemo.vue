580e1585f80595e4e9ac85ab459f302b3241d474
kanadafa <ellis89941999@gmail.com>
2023-02-28 23:29:12 +0800

    feat: add phaser game

--- src/views/PhaserDemo.vue
+++ src/views/PhaserDemo.vue
@@ -4,7 +4,21 @@
       <p>這個網頁遊戲的玩法非常簡單，就是中間的塔不可以被四周隨機生成的魔物攻擊到，總共有三個關卡 <br>
         後續的關卡魔物攻擊力會變得更高，在地圖上也更不好看清楚，塔的血量也會變少，就是一款簡單而快速的遊戲
       </p>
-      
+      <div class="iframe-wrapper">
+        <b-embed
+          type="iframe"
+          aspect="16by9"
+          scrolling="no"
+          src="https://kanadafa.github.io/phaser-game/"
+        ></b-embed>
+      </div>
     </div>
 </template>
-  
+<style scoped lang="scss">
+.iframe-wrapper {
+  max-width: 1100px;
+  margin: 0 auto;
+  padding-bottom: 50px;;
+}
+
+</style>
