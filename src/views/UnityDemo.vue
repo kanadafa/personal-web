b14f92625049ade1ceb450e958a15cc0ddb206dd
kanadafa <ellis89941999@gmail.com>
2023-10-04 13:00:13 +0800

    change 22

--- src/views/UnityDemo.vue
+++ src/views/UnityDemo.vue
@@ -9,7 +9,7 @@
     </p>
     <div class="videounity">
       <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/g8zIvafYGdE"></b-embed>
-      <p>製作感想:這主題我碰程式碼的部分不多，去尋找素材，劇情和建置場地等等的工作</p>
+      <p>個人部分:這個主題我主要是去尋找場地素材，音效素材以及後期影片製作</p>
     </div>
     <h2>第二個作品:zombie</h2>
     <p>這個遊戲就比較沒有那麼多的劇情，就只是雙人玩家可以一起去遊玩在同一個裝置上的小遊戲，構思有點像是旋風快打這類的雙人競技遊戲 <br>
@@ -18,9 +18,7 @@
     <div class="videounity">
       <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/y6G4FB5aHz0"></b-embed>
     </div>
-    <p>製作感想:這個專題我每個方面都有碰到一點，版面的設計製作等等和一些簡單的程式邏輯，我覺得畫面切割剛開始就是一個不簡單的挑戰 <br>
-      但最後透過老師上課的教導都有做出來，感覺很棒!
-    </p>
+    <p>個人部分:這個專題我主要是製作遊戲介面的UI以及按鈕操控，後製的音效以及影片製作</p>
   </div>
 </template>
 <style scoped lang="scss">
