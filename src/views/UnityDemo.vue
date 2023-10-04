d298c8b2bfd516fa36678422b5450a582794b184
kanadafa <ellis89941999@gmail.com>
2023-10-04 13:18:52 +0800

    change 23

--- src/views/UnityDemo.vue
+++ src/views/UnityDemo.vue
@@ -9,7 +9,7 @@
     </p>
     <div class="videounity">
       <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/g8zIvafYGdE"></b-embed>
-      <p>個人部分:這個主題我主要是去尋找場地素材，音效素材以及後期影片製作</p>
+      <p>個人部分:尋找場地素材，音效素材以及後期影片製作</p>
     </div>
     <h2>第二個作品:zombie</h2>
     <p>這個遊戲就比較沒有那麼多的劇情，就只是雙人玩家可以一起去遊玩在同一個裝置上的小遊戲，構思有點像是旋風快打這類的雙人競技遊戲 <br>
@@ -18,7 +18,7 @@
     <div class="videounity">
       <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/y6G4FB5aHz0"></b-embed>
     </div>
-    <p>個人部分:這個專題我主要是製作遊戲介面的UI以及按鈕操控，後製的音效以及影片製作</p>
+    <p>個人部分:製作遊戲介面的UI以及按鈕操控，後製的音效以及影片製作</p>
   </div>
 </template>
 <style scoped lang="scss">
--- src/views/WebDemo.vue
+++ src/views/WebDemo.vue
@@ -5,12 +5,12 @@
       此作品為小組共同開發，主要目的為讓屋主(客戶)以及清潔員(工作者)可以互相媒合的一個網站 <br>
       此網站的發想契機像外送平台或是共乘平台可以讓擁有資源或是短期需要此資源的人可以在互換報酬的原則下互惠 <br>
     </p>
+    <p>個人部分:製作外部的網頁連接，例如About us 等等的外部網頁製作，影片製作</p>
     <h2>以下是此網站的介紹影片</h2>
     <div class="video-wrapper">
       <b-embed type="iframe" aspect="16by9" src="https://www.youtube.com/embed/fDdUa-0hfRw"
        ></b-embed>
     </div>
-    <p>個人部分:製作外部的網頁連接，例如About us 等等的外部網頁製作，影片製作</p>
 
   </div>
 </template>
