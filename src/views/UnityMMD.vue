4e0397a197ff1cf49386d92c4686d2a7bdb943f4
kanadafa <ellis89941999@gmail.com>
2023-10-23 13:55:58 +0800

    change 25

--- src/views/UnityMMD.vue
+++ src/views/UnityMMD.vue
@@ -2,7 +2,7 @@
     <div class="about">
       <h1>Unity 使用 Vuforia 套用 MMD</h1>
       <p>
-        此作品為小組一起共同製作，主要是使用Unity套用MMD4mecanim套件來進行MMD模型五島套用後再透過Vuforia進行AR投影 <br>
+        此作品為小組一起共同製作，主要是使用Unity套用MMD4mecanim套件來進行MMD模型舞蹈套用後再透過Vuforia進行AR投影 <br>
         角色模型可以在對應的卡紙上面出現，然後再用音樂圖片來進行相對應的音樂播放與模型動作舞蹈
       </p>
       <p>個人部分:研究MMD4mecanim套件如何套入模型後再套進Vuforia來進行AR的互動，後期影片字幕</p>
